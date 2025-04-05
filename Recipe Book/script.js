1b7d2d1394d3f3407cde25f489c4705a986aee11
SaiPrasathS03 <148991466+SaiPrasathS03@users.noreply.github.com>
2025-04-05 17:24:25 +0530

    Add files via upload

---
+++ Recipe Book/script.js
@@ -0,0 +1,48 @@
+async function fetchDetails(){
+    const response = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=");
+    const content = await response.json();
+    console.log(content.meals);
+    displayRecipes(content.meals);
+}
+fetchDetails()
+function displayRecipes(meals){
+    var recipeContainer = document.getElementById("recipe-container");
+    meals.forEach(meal => {
+
+        let recipeName = document.createElement("div");
+        recipeName.className="recipe-name";
+        recipeName.innerHTML =  `
+            <img class="" src="${meal.strMealThumb}" alt="">
+            <h3>${meal.strMeal}</h3>
+        `;
+        let recipeIng = document.createElement("div");
+        recipeIng.className="recipe-ing";
+        const ingredientsList = document.createElement("ol");
+
+        for (let i = 1; i <= 20; i++) {
+            const ingredientKey = `strIngredient${i}`;
+            const measureKey = `strMeasure${i}`;
+            const ingredient = meal[ingredientKey];
+            const measure = meal[measureKey];
+
+            if (ingredient && ingredient.trim() !== "") {
+                const listItem = document.createElement("li");
+                listItem.classList.add("ingredient-item");
+                listItem.textContent = `${ingredient} - ${measure}`;
+                ingredientsList.appendChild(listItem);
+            }
+        }
+        recipeIng.appendChild(ingredientsList);
+        let recipeSteps = document.createElement("div");
+        recipeSteps.className="recipe-steps";
+        recipeSteps.innerHTML =  `
+            <p>${meal.strInstructions}</p>
+        `;
+        
+
+        recipeContainer.appendChild(recipeName);
+        recipeContainer.appendChild(recipeIng);
+        recipeContainer.appendChild(recipeSteps);
+
+    });
+}
